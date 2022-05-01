726eb05c0e25e5f98bf24284df6858dcca1f496b
lauralardies <lrodrlar@myuax.com>
2022-05-01 18:04:55 +0200

    profe ayuda no sabemos porque lee 413963 lineas

--- analisis.py
+++ analisis.py
@@ -20,27 +20,27 @@ else:
     pacientes = []
 
     for linea in lineas[1:]:
-        paciente = {}
-        linea = linea.strip()
-        campos = linea.split(",")
-        for i in range(len(columnas)):
-            if columnas[i] in seleccion:
-                paciente[traduccion[columnas[i]]] = campos[i]
-            else:
-                print ("No esta")
-            pacientes.append(paciente)
+        if linea or any(linea) or any(field.strip() for field in linea):            
+            paciente = {}
+            linea = linea.strip()
+            campos = linea.split(",")
+            for i in range(len(columnas)):
+                if columnas[i] in seleccion:
+                    paciente[traduccion[columnas[i]]] = campos[i]
+                pacientes.append(paciente)
 
     # Ahora analizamos los datos ordenados.
 
     n = len(pacientes) # Número de pacientes en total.
 
+    print ("Numero de pacientes:" + str(n))
     m = 0 # Número de pacientes que siguen online a MedCamp.
     l = 0 # Número de pacientes que siguen en LinkedIn a MedCamp.
     t = 0 # Número de pacientes que siguen en Twitter a Medcamp.
     f = 0 # Número de pacientes que siguen en Facebook a Medcamp.
-    ciudades = {} # Diccionario de las ciudades.
-    trabajo = {} # Diccionario del trabajo.
-
+    ciudades = {} # Diccionario de las ciudades con el número de pacientes que vive en cada una de ellas.
+    trabajo = {} # Diccionario de los trabajos con el número de pacientes que se dedican a ello.
+    
     for paciente in pacientes:
         # Análisis del seguimiento en redes sociales.
         if paciente["Seguidor_Online"] != "0":
@@ -51,17 +51,17 @@ else:
             t = t + 1
         if paciente["Comparte_Facebook"] != "0":
             f = f + 1
-        # Análisis ciudades.
+        # Análisis de las ciudades.
         if paciente["Tipo_Ciudad"] not in ciudades:
             ciudades[paciente["Tipo_Ciudad"]] = 1
         else:
             ciudades[paciente["Tipo_Ciudad"]] = ciudades[paciente["Tipo_Ciudad"]] + 1
-        # Análisis trabajos.
+        # Análisis de los trabajos.
         if paciente["Categoria_Empleado"] not in trabajo:
             trabajo[paciente["Categoria_Empleado"]] = 1
         else:
             trabajo[paciente["Categoria_Empleado"]] = trabajo[paciente["Categoria_Empleado"]] + 1
-
+        
     del ciudades[""] # Eliminamos aquellos casos en los que no esté especificada la ciudad del paciente.
     del trabajo[""] # Eliminamos aquellos casos en los que no esté especificado el trabajo del paciente.
 
@@ -76,17 +76,45 @@ else:
     plt.show() # Comando que muestra la gráfica.
 
     # 2º Gráfica de barras de las ciudades en relación con los pacientes.
-
     plt.bar(ciudades.keys(), ciudades.values(), color="green") # La información que aparecerá en los ejes.
     plt.xlabel("Tipos de Ciudades") # Título que explica los datos del eje X.
     plt.ylabel("Número de pacientes") # Título que explica los datos del eje Y.
     plt.title("Relación Paciente - Ciudad") # Título de la gráfica.
     plt.show()
 
-    # 3º Gráfica de barras de los trabajos en relación con los pacientes.
-
+    # 3º Gráfica de barras (horizontal) de los trabajos en relación con los pacientes.
     plt.barh(list(trabajo.keys()), trabajo.values(), color="pink") # La información que aparecerá en los ejes.
     plt.ylabel("Trabajos") # Título que explica los datos del eje Y.
     plt.xlabel("Número de pacientes") # Título que explica los datos del eje X.
     plt.title("Relación Paciente - Trabajo") # Título de la gráfica.
     plt.show()
+
+    # A parir de ahora, las variables que vamos a analizar las guardaremos en diccionarios que tienen rangos (Ej. de 60 a 70 años va a ser una entrada del diccionario "edades").
+    # Por lo tanto, para determinar estos rangos vamos a tener que calcular el mínimo y máximo de estas variables.
+
+    #valores = [paciente["Edad"] for paciente in pacientes] # Aquí guardamos una lista con todas las edades de los pacientes.
+    #valores[:] = (valor for valor in valores if valor != "None") # Eliminamos todas las edades desconocidas, que en nuestro archivo .csv están marcadas como "None".
+    #edad_min = int(min(valores))
+    #edad_max = int(max(valores))
+
+    # Quiero hacer 5 rangos.
+    #rango = (edad_max - edad_min)//5
+    #x = edad_min
+    #keys = [] # Creamos una lista vacía con los rangos
+
+    #for i in range(5):
+    #    key = "[" + str(x) + "-" + str(x+rango) + "]"
+    #    x = x + rango
+    #    keys.append(key)
+
+    #values = [0, 0, 0, 0, 0]
+
+    #for valor in valores:
+    #    for i in range(5):
+    #        if int(valor) < (edad_min + (i + 1)*rango):
+    #            values[i] = values[i] + 1
+    #            break
+    
+    #print(keys)
+    #print(values)
+    print(n)
