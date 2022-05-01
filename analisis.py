b267482da98fe3eaf8b5a444ca8a65eb69e3f98b
lauralardies <lrodrlar@myuax.com>
2022-05-01 11:33:16 +0200

    optimizamos el código

--- analisis.py
+++ analisis.py
@@ -38,8 +38,11 @@ else:
     l = 0 # Número de pacientes que siguen en LinkedIn a MedCamp.
     t = 0 # Número de pacientes que siguen en Twitter a Medcamp.
     f = 0 # Número de pacientes que siguen en Facebook a Medcamp.
+    ciudades = {} # Diccionario de las ciudades.
+    trabajo = {} # Diccionario del trabajo.
 
     for paciente in pacientes:
+        # Análisis del seguimiento en redes sociales.
         if paciente["Seguidor_Online"] != "0":
             m = m + 1
         if paciente["Comparte_LinkedIn"] != "0":
@@ -48,9 +51,23 @@ else:
             t = t + 1
         if paciente["Comparte_Facebook"] != "0":
             f = f + 1
+        # Análisis ciudades.
+        if paciente["Tipo_Ciudad"] not in ciudades:
+            ciudades[paciente["Tipo_Ciudad"]] = 1
+        else:
+            ciudades[paciente["Tipo_Ciudad"]] = ciudades[paciente["Tipo_Ciudad"]] + 1
+        # Análisis trabajos.
+        if paciente["Categoria_Empleado"] not in trabajo:
+            trabajo[paciente["Categoria_Empleado"]] = 1
+        else:
+            trabajo[paciente["Categoria_Empleado"]] = trabajo[paciente["Categoria_Empleado"]] + 1
 
-    # Vamos a realizar una gráfica de barras con los datos analizados hasta ahora.
+    del ciudades[""] # Eliminamos aquellos casos en los que no esté especificada la ciudad del paciente.
+    del trabajo[""] # Eliminamos aquellos casos en los que no esté especificado el trabajo del paciente.
 
+    # Vamos a realizar las gráficas de barras con los datos analizados hasta ahora.
+
+    # 1º Gráfica del seguimiento Online.
     plt.bar(["Seguidor\n Online", "Comparte en\n LinkedIn", "Comparte en\n Twitter", "Comparte en\n Facebook"], [m, l, t, f]) # La información que aparecerá en los ejes.
     plt.xlabel("Actividad en las redes sociales") # Título que explica los datos del eje X.
     plt.ylim(0, (max(m, l, t, f)*1.5)) # Determinamos a nuestro gusto el límite del eje Y.
@@ -58,18 +75,7 @@ else:
     plt.title("Relación Paciente - Redes Sociales") # Título de la gráfica.
     plt.show() # Comando que muestra la gráfica.
 
-    # Creamos un diccionario de las ciudades de los pacientes.
-
-    ciudades = {}
-    for paciente in pacientes:
-        if paciente["Tipo_Ciudad"] not in ciudades:
-            ciudades[paciente["Tipo_Ciudad"]] = 1
-        else:
-            ciudades[paciente["Tipo_Ciudad"]] = ciudades[paciente["Tipo_Ciudad"]] + 1
-
-    del ciudades[""] # Eliminamos aquellos casos en los que no esté especificada la ciudad del paciente.
-
-    # Pintamos gráfica de barras de las ciudades en relación con los pacientes.
+    # 2º Gráfica de barras de las ciudades en relación con los pacientes.
 
     plt.bar(ciudades.keys(), ciudades.values()) # La información que aparecerá en los ejes.
     plt.xlabel("Tipos de Ciudades") # Título que explica los datos del eje X.
@@ -77,18 +83,7 @@ else:
     plt.title("Relación Paciente - Ciudad") # Título de la gráfica.
     plt.show()
 
-    # Creamos un diccionario de los trabajos de los pacientes.
-
-    trabajo = {}
-    for paciente in pacientes:
-        if paciente["Categoria_Empleado"] not in trabajo:
-            trabajo[paciente["Categoria_Empleado"]] = 1
-        else:
-            trabajo[paciente["Categoria_Empleado"]] = trabajo[paciente["Categoria_Empleado"]] + 1
-
-    del trabajo[""] # Eliminamos aquellos casos en los que no esté especificado el trabajo del paciente.
-
-    # Pintamos gráfica de barras de los trabajos en relación con los pacientes.
+    # 3º Gráfica de barras de los trabajos en relación con los pacientes.
 
     plt.barh(list(trabajo.keys()), trabajo.values()) # La información que aparecerá en los ejes.
     plt.ylabel("Trabajos") # Título que explica los datos del eje Y.
