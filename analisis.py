92ee044dfe5a32552660039cac3593b297ed8d42
lauralardies <lrodrlar@myuax.com>
2022-05-02 19:44:06 +0200

    analizados los datos de los campamentos

--- analisis.py
+++ analisis.py
@@ -31,17 +31,17 @@ def leer_csv(archivo, seleccion, traduccion):
     
 def seguimiento_online(pacientes):
 
-    online = {"m" : 0, "l" :0, "t" : 0, "f" : 0}
+    online = {"Seguidor Online" : 0, "Comparte LinkedIn" :0, "Comparte Twitter" : 0, "Comparte Facebook" : 0}
 
     for paciente in pacientes:
-        if paciente["Seguidor_Online"] != "0":
-            online["m"] = online["m"] + 1
-        if paciente["Comparte_LinkedIn"] != "0":
-            online["l"] = online["l"] + 1
-        if paciente["Comparte_Twitter"] != "0":
-            online["t"] = online["t"] + 1
-        if paciente["Comparte_Facebook"] != "0":
-            online["f"] = online["f"] + 1
+        if paciente["Seguidor Online"] != "0":
+            online["Seguidor Online"] = online["Seguidor Online"] + 1
+        if paciente["Comparte LinkedIn"] != "0":
+            online["Comparte LinkedIn"] = online["Comparte LinkedIn"] + 1
+        if paciente["Comparte Twitter"] != "0":
+            online["Comparte Twitter"] = online["Comparte Twitter"] + 1
+        if paciente["Comparte Facebook"] != "0":
+            online["Comparte Facebook"] = online["Comparte Facebook"] + 1
 
     return online
 
@@ -50,10 +50,10 @@ def tipos_ciudades(pacientes):
     ciudades = {} # Diccionario de las ciudades con el número de pacientes que vive en cada una de ellas.
 
     for paciente in pacientes:
-        if paciente["Tipo_Ciudad"] not in ciudades:
-                ciudades[paciente["Tipo_Ciudad"]] = 1
+        if paciente["Tipo Ciudad"] not in ciudades:
+                ciudades[paciente["Tipo Ciudad"]] = 1
         else:
-            ciudades[paciente["Tipo_Ciudad"]] = ciudades[paciente["Tipo_Ciudad"]] + 1
+            ciudades[paciente["Tipo Ciudad"]] = ciudades[paciente["Tipo Ciudad"]] + 1
 
     del ciudades[""] # Eliminamos aquellos casos en los que no esté especificada la ciudad del paciente.
 
@@ -64,10 +64,10 @@ def tipo_trabajo(pacientes):
     trabajo = {} # Diccionario de los trabajos con el número de pacientes que se dedican a ello.
 
     for paciente in pacientes:
-        if paciente["Categoria_Empleado"] not in trabajo:
-            trabajo[paciente["Categoria_Empleado"]] = 1
+        if paciente["Categoria Empleado"] not in trabajo:
+            trabajo[paciente["Categoria Empleado"]] = 1
         else:
-            trabajo[paciente["Categoria_Empleado"]] = trabajo[paciente["Categoria_Empleado"]] + 1
+            trabajo[paciente["Categoria Empleado"]] = trabajo[paciente["Categoria Empleado"]] + 1
 
     del trabajo[""] # Eliminamos aquellos casos en los que no esté especificado el trabajo del paciente.
 
@@ -87,34 +87,57 @@ def cantidad_ingresos(pacientes):
 
     return ingresos
 
-def edades_o_educacion(pacientes, variable):
+def dict_con_rango(datos, variable, n_rango):
 
-    valores = [paciente[variable] for paciente in pacientes] # Aquí guardamos una lista con todos los valores de la variable.
-    valores[:] = (valor for valor in valores if valor != "None") # Eliminamos todas los valores desconocids, que en nuestro archivo .csv están marcadas como "None".
+    valores = [dato[variable] for dato in datos] # Aquí guardamos una lista con todos los valores de la variable.
+    valores[:] = (valor for valor in valores if valor != "None") # Eliminamos todas los valores desconocidos, que en nuestro archivo .csv están marcados como "None".
     # Seguidamente buscamos el máximo y el mínimo para encontrar el rango.
     valor_min = float(min(valores, key=float))
     valor_max = float(max(valores, key=float))
-    # Quiero crear 5 rangos de distintos de esta variable. Estos rangos se generan entre el máximo y el mínimo.
-    rango = (valor_max - valor_min)//5
+    # Quiero crear n rangos de distintos de esta variable. Estos rangos se generan entre el máximo y el mínimo.
+    rango = (valor_max - valor_min)/n_rango
     x = valor_min
     keys = [] # Creamos una lista vacía con los rangos.
 
-    for i in range(5): # En este bucle creamos los rangos de ambas variables(edad y educación).
+    for i in range(n_rango): # En este bucle creamos los rangos de ambas variables(edad y educación).
         key = str(x) + "-" + str(x + rango)
         x = x + rango
         keys.append(key)
 
-    values = [0, 0, 0, 0, 0] # Lista del número de pacientes por rango (ponemos 5 ceros al haber establecido que queremos 5 rangos).
+    values = [0] * n_rango # Lista del número de pacientes por rango (ponemos 5 ceros al haber establecido que queremos 5 rangos).
 
     for valor in valores: # Bucle que estudia la edad de cada paciente
-        for i in range(5):
+        for i in range(n_rango):
             if float(valor) < (valor_min + (i + 1) * rango):
                 values[i] = values[i] + 1
                 break
 
     return dict(zip(keys, values))
 
-def graficas(variable, horizontal):
+def puntuacion_puestos(usuarios):
+
+    stalls = {} # Diccionario con el número de stalls visitados por cada paciente.
+
+    for usuario in usuarios:
+        if usuario["Número de puestos visitados"] not in stalls:
+            stalls[usuario["Número de puestos visitados"]] = 1
+        else:
+            stalls[usuario["Número de puestos visitados"]] = stalls[usuario["Número de puestos visitados"]] + 1
+
+    return stalls
+
+def puesto_masvisitado(usuarios):
+    stall = {} # Diccionario con el número de stalls visitados por cada paciente.
+
+    for usuario in usuarios:
+        if usuario["Número último puesto visitado"] not in stall:
+            stall[usuario["Número último puesto visitado"]] = 1
+        else:
+            stall[usuario["Número último puesto visitado"]] = stall[usuario["Número último puesto visitado"]] + 1
+
+    return stall
+
+def graficas(variable, titulo, horizontal):
 
     if horizontal == True: # Con la variable horizontal marco si quiero realizar una gráfica de barras horizontales o verticales.
         plt.barh(list(variable.keys()), variable.values(), color = np.random.rand(3,)) # La información que aparecerá en los ejes.
@@ -124,23 +147,49 @@ def graficas(variable, horizontal):
         plt.bar(variable.keys(), variable.values(), color = np.random.rand(3,)) # La información que aparecerá en los ejes.
         plt.ylabel("Número de pacientes") # Título que explica los datos del eje Y.
 
+    plt.title(titulo)
     plt.show()
 
 # ----- L E C T U R A   D E   A R C H I V O -----
 # Primero analizamos el fichero y creamos una lista de diccionarios.
 
+# Fichero Patient_Profile.csv
 perfil_paciente = "health-care analytics\Train\Patient_Profile.csv"
 var_paciente = ["Patient_ID", "Online_Follower", "LinkedIn_Shared", "Twitter_Shared", "Facebook_Shared", "Income", "Education_Score", "Age", "First_Interaction", "City_Type", "Employer_Category"]
-traduccion_var = {"Patient_ID":"ID_Paciente", "Online_Follower":"Seguidor_Online", "LinkedIn_Shared":"Comparte_LinkedIn", "Twitter_Shared":"Comparte_Twitter", "Facebook_Shared":"Comparte_Facebook", "Income":"Ingresos", "Education_Score":"Educación", "Age":"Edad", "First_Interaction":"Primera_Iteracción", "City_Type":"Tipo_Ciudad", "Employer_Category":"Categoria_Empleado"}
-
+traduccion_var = {"Patient_ID" : "ID Paciente", "Online_Follower" : "Seguidor Online", "LinkedIn_Shared" : "Comparte LinkedIn", "Twitter_Shared" : "Comparte Twitter", "Facebook_Shared" : "Comparte Facebook", "Income" : "Ingresos", "Education_Score" : "Educación", "Age" : "Edad", "First_Interaction" : "Primera Iteracción", "City_Type" : "Tipo Ciudad", "Employer_Category" : "Categoria Empleado"}
 pacientes = leer_csv(perfil_paciente, var_paciente, traduccion_var)
 
-# ----- G R Á F I C O S -----
+# Fichero First_Health_Camp_Attended.csv
+info_campamento1 = "health-care analytics\Train\First_Health_Camp_Attended.csv"
+var_campamento1 = ["Patient_ID", "Health_Camp_ID", "Donation", "Health_Score"]
+traduccion_var1 = {"Patient_ID":"ID Paciente", "Health_Camp_ID" : "ID Campamento", "Donation" : "Donación", "Health_Score" : "Puntuación Salud"}
+usuarios_1 = leer_csv(info_campamento1, var_campamento1, traduccion_var1)
+
+# Fichero Second_Health_Camp_Attended.csv
+info_campamento2 = "health-care analytics\Train\Second_Health_Camp_Attended.csv"
+var_campamento2 = ["Patient_ID", "Health_Camp_ID", "Health Score"]
+traduccion_var2 = {"Patient_ID":"ID Paciente", "Health_Camp_ID" : "ID Campamento", "Health Score" : "Puntuación Salud"}
+usuarios_2 = leer_csv(info_campamento2, var_campamento2, traduccion_var2)
+
+# Fichero Third_Health_Camp_Attended.csv
+info_campamento3 = "health-care analytics\Train\Third_Health_Camp_Attended.csv"
+var_campamento3= ["Patient_ID", "Health_Camp_ID", "Number_of_stall_visited", "Last_Stall_Visited_Number"]
+traduccion_var3 = {"Patient_ID":"ID Paciente", "Health_Camp_ID" : "ID Campamento", "Number_of_stall_visited" : "Número de puestos visitados", "Last_Stall_Visited_Number" : "Número último puesto visitado"}
+usuarios_3 = leer_csv(info_campamento3, var_campamento3, traduccion_var3)
+
+# ----- G R Á F I C A S -----
 # Vamos a realizar las gráficas de barras con los datos analizados hasta ahora.
 
-graficas(seguimiento_online(pacientes), False)
-graficas(tipos_ciudades(pacientes), False)
-graficas(tipo_trabajo(pacientes), True)
-graficas(cantidad_ingresos(pacientes), False)
-graficas(edades_o_educacion(pacientes, "Edad"), False)
-graficas(edades_o_educacion(pacientes, "Educación"), False)
+# Datos de pacientes.
+graficas(seguimiento_online(pacientes), "Pacientes y su seguimiento Online de MedCamp", True)
+graficas(tipos_ciudades(pacientes), "¿En qué ciudades viven nuestros pacientes?", False)
+graficas(tipo_trabajo(pacientes), "¿En qué trabajan los pacientes de MedCamp?", True)
+graficas(cantidad_ingresos(pacientes), "Relación Ingresos - Paciente", False)
+graficas(dict_con_rango(pacientes, "Edad", 5), "Edad de los pacientes de MedCamp", False)
+graficas(dict_con_rango(pacientes, "Educación", 5), "Puntuación de la educación recibida por lo pacientes", True)
+
+# Datos de campamentos.
+graficas(dict_con_rango(usuarios_1, "Puntuación Salud", 12), "Puntuación de Salud del Primer Campamento de Medcamp", True)
+graficas(dict_con_rango(usuarios_2, "Puntuación Salud", 12), "Puntuación de Salud del Segundo Campamento de Medcamp", True)
+graficas(puntuacion_puestos(usuarios_3), "Número total de puestos visitados por cada paciente en el Tercer Campamento", False)
+graficas(puesto_masvisitado(usuarios_3), "Puesto más visitado como el último puesto de los pacientes", False)
